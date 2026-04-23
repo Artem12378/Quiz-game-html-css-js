eb7c69b450d9a037f7ef38cd8f6e4c943fba1faa
Artem12378 <susidko.2012@mail.ru>
2026-04-23 06:10:31 +0300

    Css styles

---
+++ index.js
@@ -0,0 +1,10 @@
+const welcomeScreen = document.getElementById('welcome-screen');
+const questionScreen = document.getElementById('question-screen');
+const resultScreen = document.getElementById('result-screen');
+
+
+
+const playerform = document.getElementById('player-form');
+
+
+const playerName = document.getElementById('player-name');
